0b8874fb7b24eb296a995befb5d08b4a65bfdd5d
MasafumiYamamoto <mythmaker3633@yahoo.co.jp>
2015-10-20 14:44:14 +0900

    mk_calc_LSI
    LSIモデルでのクエリに対するトピック計算

---
+++ calcsim_LSI.py
@@ -0,0 +1,42 @@
+from gensim import corpora, models, similarities
+import csv
+import textedit
+pas=""
+
+dictionary = corpora.Dictionary.load(pas+"nNV/nNVreview.dict")
+corpus = corpora.MmCorpus(pas+"nNV/nNVreview.mm")
+
+#use LSI
+lsi = models.LsiModel.load(pas+"nNV/nNVreview.lsi")
+
+
+#calc topic sim
+header=[]
+header.append("rev_id")
+header.append("bus_id")
+for num in range(1,51):
+header.append("t"+str(num))
+
+wfile=open("NVrevtopic_LSI.csv","wb")
+writer=csv.writer(wfile)
+writer.writerow(header)
+
+ifile=open("NVreview.csv","r")
+idata=csv.reader(ifile)
+idata.next()
+k=0
+for line in idata:
+wlist=[]
+wlist.append(line[0])
+wlist.append(line[1])
+doc=textedit.textedit(line[2])
+vec_bow = dictionary.doc2bow(doc.lower().split())
+vec_lsi = lsi[vec_bow]
+slist=[0]*50
+for num in range(0,len(vec_lda)):
+	slist[vec_lsi[num][0]]=vec_lsi[num][1]
+wlist=wlist+slist
+writer.writerow(wlist)
+
+ifile.close()
+wfile.close()
