a9ed1c6314384b82927a7be0665357f394b325cd
MasafumiYamamoto <mythmaker3633@yahoo.co.jp>
2015-10-20 17:13:35 +0900

    for LSI
    LSIを使えるようにするためにいろいろ追加

--- calcsim_LSI.py
+++ calcsim_LSI.py
@@ -17,10 +17,11 @@ header.append("bus_id")
 for num in range(1,51):
 header.append("t"+str(num))
 
-wfile=open("NVrevtopic_LSI.csv","wb")
+wfile=open("nNVrevtopic_LSI.csv","wb")
 writer=csv.writer(wfile)
 writer.writerow(header)
 
+#NVreview:[review_id,user_id,bus_id,stars,date,text]
 ifile=open("NVreview.csv","r")
 idata=csv.reader(ifile)
 idata.next()
@@ -28,8 +29,8 @@ k=0
 for line in idata:
 wlist=[]
 wlist.append(line[0])
-wlist.append(line[1])
-doc=textedit.textedit(line[2])
+wlist.append(line[2])
+doc=textedit.textedit(line[5])
 vec_bow = dictionary.doc2bow(doc.lower().split())
 vec_lsi = lsi[vec_bow]
 slist=[0]*50
---
+++ writetopicword_LSI.py
@@ -0,0 +1,28 @@
+import csv
+import gensim
+
+#file pass
+pas=""
+#prepare
+dictionary=gensim.corpora.Dictionary.load(pas+"/nNV/nNVreview.dict")
+corpus=gensim.corpora.MmCorpus(pas+"nNVreview.mm")
+#use LSI
+lsi=gensim.models.LsiModel.load(pas+"nNV/nNVreview.lsi")
+
+wfile=open("topicword_LSI.csv","wb")
+writer=csv.writer(wfile)
+header=["Topic"]
+for i in range(0,10):
+    header.append("ww"+str(i).zfill(2))
+    header.append("tw"+str(i).zfill(2))
+writer.writerow(header)
+
+for topic in range(0,lsi.num_topics):
+    wlist=[]
+    wlist.append(str(topic).zfill(2))
+    word=lsi.print_topic(topic)
+    word=word.replace("+"," ")
+    word=word.replace("*"," ")
+    wlist=wlist+word.split()
+    writer.writerow(wlist)
+wfile.close()
