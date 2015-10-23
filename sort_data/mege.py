32b473eace125ac6cd2d3affcf474c85b566f488
MasafumiYamamoto <mythmaker3633@yahoo.co.jp>
2015-10-23 17:08:49 +0900

    subrev
    トピック和の計算時に省くファイルを受付るようにした

--- sort_data/mege.py
+++ sort_data/mege.py
@@ -1,31 +1,53 @@
 import csv
 import glob
 import numpy
+import collections
 
 print "file pass"
 spas=raw_input()
 print "topicnum"
 topicnum=int(raw_input())
 
-wfile=open("mergeLDA"+str(topicnum)+".csv","wb")
+rfile=open("subrev_1000.csv","r")
+rdata=csv.reader(rfile)
+rdata.next()
+rlist=collections.Counter()
+for line in rdata:
+    rlist[line[0]]=1
+print "rlist",len(rlist)
+
+wfile=open("mergeLDA_r"+str(topicnum)+".csv","wb")
 writer=csv.writer(wfile)
 header=["bus_id"]
 for num in range(0,int(topicnum)):
     header.append("t"+str(num).zfill(2))
 writer.writerow(header)
 slist=glob.glob(spas+"/*")
-snum=0
+subnum=0
 for shop in slist:
     wlist=[]
     tlist=numpy.array([0]*topicnum)
     ifile=open(shop,"r")
     idata=csv.reader(ifile)
+    snum=0
     for line in idata:
         bus_id=line[2]
-        tlist=tlist+numpy.array(map(float,line[5:]))
+        user_id=line[1]
+        if(user_id not in rlist):
+            if(int(line[3])>3):
+                tlist=tlist+numpy.array(map(float,line[5:]))
+                snum=snum+1
+            elif(int(line[3])<3):
+                tlist=tlist-numpy.array(map(float,line[5:]))
+                snum=snum+1
+        else:
+            subnum=subnum+1
+    if(snum>0):
+        tlist=tlist/snum
     writer.writerow([bus_id]+list(tlist))
     ifile.close()
     snum=snum+1
     if(snum%1000==0):
         print snum
+print "subnum",subnum
 wfile.close()
