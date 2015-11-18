4857a9338f324360b530c4f4905d33d431b97ac6
MasafumiYamamoto <mythmaker3633@yahoo.co.jp>
2015-11-18 11:52:58 +0900

    shange sort_data
    LSI->LDA
    使うたびに適宜変更すること？

--- sort_data/sort_data.py
+++ sort_data/sort_data.py
@@ -14,8 +14,8 @@ tnum_=raw_input()
 bnum_=6
 rflag_=0
 tflag_=0
-infile_="NVrev_LSI_o4b6t"+str(tnum_)+".csv"
-model_="LSI"
+infile_="NVrev_LDA_o4b6t"+str(tnum_)+".csv"
+model_="LDA"
 
 print "start",time.ctime()
 mkeachbus_d.main(rootpath_,tnum_,bnum_,rflag_,tflag_,infile_,model_)
